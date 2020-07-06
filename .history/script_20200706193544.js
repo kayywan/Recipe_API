a40b13a3f5a118c5ad01b3a7946f1f9985502e6d
LLenroc <nc_2@hotmail.com>
2020-07-06 19:59:11 -0400

    added card body to search results that will hold all the recipes found

--- .history/script_20200706193544.js
+++ .history/script_20200706193544.js
@@ -3,7 +3,7 @@
 function buildQueryURL() {
     var queryURL = "https://tasty.p.rapidapi.com/recipes/detail";
 
-    var queryParams = { "api-key": "ac032b7765msh7b7ea8d251892bbp18630ejsnfccfef5696ae"};
+    vary queryParams = { "api-key": "ac032b7765msh7b7ea8d251892bbp18630ejsnfccfef5696ae"};
 
     queryParams.q = $("#ingredient");
 }
