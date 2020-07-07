e03e76e69d20a56636d0a308c277005a1526cc00
LLenroc <nc_2@hotmail.com>
2020-07-06 21:49:26 -0400

    adding to javascript

--- .history/script_20200706213212.js
+++ .history/script_20200706213212.js
@@ -2,13 +2,13 @@ $(document).ready (function() {
 });
 
 $('button').on('click', function () {
-    var queryURL = "https://tasty.p.rapidapi.com/recipes/detail/search?q=" +
-        ingredient + "&api_key=ac032b7765msh7b7ea8d251892bbp18630ejsnfccfef5696ae";
+    var ingredient = $(this).attr('ingredient');
+    var queryURL = "https://tasty.p.rapidapi.com/recipes/detail";
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function (response) {
-
+    
 })
     
     
--- .history/script_20200706213600.js
+++ .history/script_20200706213600.js
@@ -2,8 +2,8 @@ $(document).ready (function() {
 });
 
 $('button').on('click', function () {
-    var queryURL = "https://tasty.p.rapidapi.com/recipes/detail/search?q=" +
-        ingredient + "&api_key=ac032b7765msh7b7ea8d251892bbp18630ejsnfccfef5696ae";
+    var ingredient = $(this).attr('ingredient');
+    var queryURL = "https://tasty.p.rapidapi.com/recipes/detail/search?q=";
 $.ajax({
     url: queryURL,
     method: "GET"
